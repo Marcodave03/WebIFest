f08f8289eb3fa63bcba318606e57d0d46bcd696f
Marcodave03 <113909971+Marcodave03@users.noreply.github.com>
2024-02-20 01:12:36 +0700

    untuk kch three js

--- src/Pages/event.tsx
+++ src/Pages/event.tsx
@@ -1,19 +1,19 @@
 import React, { useEffect } from 'react';
 import '../App.css';
 import Navbar from '../Components/navbar';
-import { initializeThree } from './logicScript/event'; // Ensure the import path matches the location of your Three.js setup file
+import { initializeThree } from './logicScript/event';
 
 const Event: React.FC = () => {
   useEffect(() => {
-    // Call the function to initialize the Three.js scene after the component mounts
-    initializeThree('eventThree','../Assets/komodo.glb');
-  }, []); // The empty dependency array ensures this effect runs only once after the initial render
+    initializeThree('eventThree','./komodo.glb');
+    //initializeThree('eventThree', 'Assets/komodo.glb');
+  }, []);
 
   return (
     <div className="Event">
-      <Navbar/>
+      <Navbar />
       <header className="Event-header">
-        <canvas id='eventThree'></canvas> {/* The canvas ID here matches the ID passed to initializeThree */}
+        <canvas id='eventThree'></canvas>
       </header>
     </div>
   );
