5de6a9bd77e045092d5e6caf4357f3fc09976588
Peperooni <kennch90@gmail.com>
2024-02-19 21:59:32 +0700

    update navbar

--- src/Components/navbar.tsx
+++ src/Components/navbar.tsx
@@ -13,11 +13,12 @@ function Navbar() {
         <span className="toggler-icon">{isSidebarOpen ? '✕' : '☰'}</span> {/* Toggle between hamburger and X icon */}
       </button>
 
-      <div className={`sidebar ${isSidebarOpen ? 'show' : ''}`} style={{width: isSidebarOpen ? '250px' : '0', transition: 'width 0.3s'}}>
+      <div className={`sidebar ${isSidebarOpen ? 'show' : ''}`} style={{width: isSidebarOpen ? 'calc(210px + 10%)' : '0', transition: 'width 0.3s', height : 'auto',paddingBottom: '5vh'}}>
         <Link to="/Home" className="sidebar-link"> Home </Link>
         <Link to="/Place" className="sidebar-link"> Place </Link>
         <Link to="/About" className="sidebar-link"> About </Link>
         <Link to="/Event" className="sidebar-link"> Event </Link>
+        <div></div>
       </div>
     </div>
   );
