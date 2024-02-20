eaff2c2adfa5b08470bbe8986a163e4032bc1980
selika <putriselika@gmail.com>
2024-02-20 21:41:14 +0700

    Update place, add page.css

--- src/Pages/place.tsx
+++ src/Pages/place.tsx
@@ -13,9 +13,33 @@ function Place() {
     <div className="Place">
       <Navbar/>
       <header className="Place-header">
-        <p data-aos="fade-up">
-          EXPLORE NUSA TENGGARA TIMUR AND SEE PLACES TO VISIT  
+        <div id="tagline">
+        <p data-aos="fade-up" id="p1">
+          EXPLORE NUSA TENGGARA TIMUR   
         </p>
+        <p data-aos="fade-up" id = "p2">
+          AND SEE PLACES TO VISIT
+        </p>
+        </div>
+
+        <div id="mv">
+          {/* Most visited */}
+          <p id="p1trending"> #trending </p>
+          <p id ="p2mvp"> MOST VISITED </p>
+          <p id ="p2mvp"> PLACES 2023 </p>
+        </div>
+        <div>
+          {/* Locations */}
+        </div>
+        <div>
+          {/* Boat type */}
+        </div>
+        <div>
+          {/* Hotels */}
+        </div>
+        <div>
+          {/* NTT Pari */}
+        </div>
       </header>
     </div>
   );
