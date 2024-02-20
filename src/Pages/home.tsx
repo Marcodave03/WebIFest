2d9ec7728aaf9aad45a5233f2333a29cbfde8712
selika <putriselika@gmail.com>
2024-02-20 14:40:32 +0700

    Update style

--- src/Pages/home.tsx
+++ src/Pages/home.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import '../App.css';
 import '../App';
-import Navbar from '../Components/navbar';
+import Navbar from '../Components/navbar';;
 
 function Home() {
+  
   return (
     <div className="Home">
-      <Navbar/>
-      <br />
-      <br />
-      <br />
+        <Navbar/>
+        <br />
+        <br />
+        <br />
+
+        {/* <header className="Home-header">
+            <p>
+                Welcome to Home / Landing
+            </p>
+        </header>
+        <button className="btn btn-primary">Click me</button>  */}
     </div>
   );
 }
--- src/Pages/place.tsx
+++ src/Pages/place.tsx
@@ -2,14 +2,19 @@ import React from 'react';
 import '../App.css';
 import '../App';
 import Navbar from '../Components/navbar';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import './Style/Page.css';
 
 function Place() {
+  AOS.init();
+
   return (
     <div className="Place">
       <Navbar/>
       <header className="Place-header">
-        <p>
-          Welcome to Place 
+        <p data-aos="fade-up">
+          EXPLORE NUSA TENGGARA TIMUR AND SEE PLACES TO VISIT  
         </p>
       </header>
     </div>
