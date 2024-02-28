1bb9f345ef04a4dfe27937a0c2af5ac7d74cbe80
selika <putriselika@gmail.com>
2024-02-28 22:47:25 +0700

    add pictures and update place

--- src/Pages/place.tsx
+++ src/Pages/place.tsx
@@ -1,45 +1,103 @@
 import React from 'react';
-import '../App.css';
 import '../App';
 import Navbar from '../Components/navbar';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import './Style/Page.css';
+import explores from '../Assets/explore.jpg';
+import padar from '../Assets/padar.jpeg';
+
+import { text } from 'stream/consumers';
+import { Container } from 'react-bootstrap';
 
 function Place() {
   AOS.init();
 
+  //style tulisan explore blabla biar di tengah rata kiri
+  const exploreSectionStyle = {
+    backgroundImage: `url(${explores})`,
+    backgroundSize: 'cover',
+    width: '100%',
+    height: '100vh',
+    margin: 0,
+    display: 'flex',
+    alignItems: 'center',
+  };
+
+  // ukuran untuk section 2 (most visited)
+  const sectionStyle = {
+    width: '1920px',
+    height: '600px'
+  }
+
+  //padar
+  const rectangleStyle = {
+    width: '444px',
+    height: '550px',
+    backgroundSize: 'cover',
+    backgroundImage: `url(${padar})`,
+    display: 'flex',
+    marginLeft: '-200px',
+    borderRadius: '20px'
+  };
+
+  const mostVisited = {
+    color: 'rgba(0, 0, 0, 0.80)',
+    textShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
+    fontFamily: 'Almarai',
+    fontSize: '80px',
+    fontStyle: 'normal',
+    fontWeight: '600',
+    lineHeight: 'normal',
+  };
+
+
   return (
     <div className="Place">
-      <Navbar/>
-      <header className="Place-header">
-        <div id="tagline">
-        <p data-aos="fade-up" id="p1">
-          EXPLORE NUSA TENGGARA TIMUR   
-        </p>
-        <p data-aos="fade-up" id = "p2">
-          AND SEE PLACES TO VISIT
-        </p>
+      <Navbar />
+      {/* Explore */}
+      <section className='explore' style={exploreSectionStyle}>
+        <div className="container">
+          <div className="row" >
+            <div className="col d-flex">
+              <div id="tagline">
+                <p data-aos="fade-up" id="p1" data-aos-duration="3000" style={{ textAlign: 'left' }}>
+                  EXPLORE NUSA
+                </p>
+                <p data-aos="fade-up" id="p2" data-aos-duration="3000" style={{ textAlign: 'left' }}>
+                  TENGGARA TIMUR
+                </p>
+                <p data-aos="fade-up" id="p3" data-aos-duration="3000" style={{ textAlign: 'left' }}>
+                  AND SEE PLACES
+                </p>
+                <p data-aos="fade-up" id="p4" data-aos-duration="3000" style={{ textAlign: 'left' }}>
+                  TO VISIT
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
+      </section>
 
-        <div id="mv">
-          {/* Most visited */}
-          <p id="p1trending"> #trending </p>
-          <p id ="p2mvp"> MOST VISITED </p>
-          <p id ="p2mvp"> PLACES 2023 </p>
+      {/* Most visited places */}
+      <section style={sectionStyle}>
+        <div className="container">
+          <div className="row">
+            <div className="col" style={{ textAlign: 'center' }}>
+              <Container> </Container>
+              <div style={rectangleStyle}></div>
+              <p style={mostVisited}> MOST VISITED </p>
+              <p style={mostVisited}> PLACES 2023 </p>
+            </div>
+          </div>
         </div>
-        <div>
-          {/* Locations */}
-        </div>
-        <div>
-          {/* Boat type */}
-        </div>
-        <div>
-          {/* Hotels */}
-        </div>
-        <div>
-          {/* NTT Pari */}
-        </div>
+      </section>
+
+
+      <header className="Place-header">
+
+
+
       </header>
     </div>
   );
