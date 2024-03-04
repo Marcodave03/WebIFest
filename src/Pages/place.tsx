8820fd58d41b89073a130b710dc7326e36d61b9b
Peperooni <kennch90@gmail.com>
2024-03-04 12:43:48 +0700

    komit

--- src/Pages/place.tsx
+++ src/Pages/place.tsx
@@ -12,8 +12,8 @@ import resort2 from '../Assets/resort-2.jpg';
 import resort3 from '../Assets/resort-3.jpg';
 import resort4 from '../Assets/resort-4.jpg';
 import resort5 from '../Assets/resort-5.jpg';
-// import { Swiper, SwiperSlide } from 'swiper/react';
-// import SwiperCore, { EffectCoverflow, Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { EffectCoverflow, Pagination } from "swiper/modules";
 import 'swiper/swiper-bundle.css';
 import 'swiper/css/effect-coverflow';
 import 'swiper/css/pagination';
@@ -216,7 +216,7 @@ function Place() {
             <p id='htlTxt'> Hotels </p>
           </div>
           <div className="row-2-htl">
-            {/* <Swiper
+            <Swiper
               effect={'coverflow'}
               grabCursor={true}
               centeredSlides={true}
@@ -246,7 +246,7 @@ function Place() {
               <SwiperSlide>
                 <img style={resortImg5} />
               </SwiperSlide>
-            </Swiper> */}
+            </Swiper>
           </div>
         </div>
       </section>
