557a6f762da91f9d6409795f33d816dfb6571790
Marcodave03 <113909971+Marcodave03@users.noreply.github.com>
2024-03-04 23:47:36 +0700

    ok

--- src/Pages/place.tsx
+++ src/Pages/place.tsx
@@ -166,7 +166,123 @@ function Place() {
       <section className="mostVisited">
         <div className="container">
           <div className="row">
-            <div className="col-6" style={rectangleStyle}></div>
+            <div className="carousel">
+              <div className="list">
+                  <div className="item">
+                      <div id='boat-img' style={tripImage}></div>
+                      <div className="content">
+                          <div className="author">LUNDEV</div>
+                          <div className="title">DESIGN SLIDER</div>
+                          <div className="topic">ANIMAL</div>
+                          <div className="des">
+                              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut sequi, rem magnam nesciunt minima placeat, itaque eum neque officiis unde, eaque optio ratione aliquid assumenda facere ab et quasi ducimus aut doloribus non numquam. Explicabo, laboriosam nisi reprehenderit tempora at laborum natus unde. Ut, exercitationem eum aperiam illo illum laudantium?
+                          </div>
+                          <div className="buttons">
+                              <button>SEE MORE</button>
+                              <button>SUBSCRIBE</button>
+                          </div>
+                      </div>
+                  </div>
+                  <div className="item">
+                      <div id='boat-img' style={tripImage}></div>
+                      <div className="content">
+                          <div className="author">LUNDEV</div>
+                          <div className="title">DESIGN SLIDER</div>
+                          <div className="topic">ANIMAL</div>
+                          <div className="des">
+                              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut sequi, rem magnam nesciunt minima placeat, itaque eum neque officiis unde, eaque optio ratione aliquid assumenda facere ab et quasi ducimus aut doloribus non numquam. Explicabo, laboriosam nisi reprehenderit tempora at laborum natus unde. Ut, exercitationem eum aperiam illo illum laudantium?
+                          </div>
+                          <div className="buttons">
+                              <button>SEE MORE</button>
+                              <button>SUBSCRIBE</button>
+                          </div>
+                      </div>
+                  </div>
+                  <div className="item">
+                    <div id='boat-img' style={tripImage}></div>
+                      <div className="content">
+                          <div className="author">LUNDEV</div>
+                          <div className="title">DESIGN SLIDER</div>
+                          <div className="topic">ANIMAL</div>
+                          <div className="des">
+                              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut sequi, rem magnam nesciunt minima placeat, itaque eum neque officiis unde, eaque optio ratione aliquid assumenda facere ab et quasi ducimus aut doloribus non numquam. Explicabo, laboriosam nisi reprehenderit tempora at laborum natus unde. Ut, exercitationem eum aperiam illo illum laudantium?
+                          </div>
+                          <div className="buttons">
+                              <button>SEE MORE</button>
+                              <button>SUBSCRIBE</button>
+                          </div>
+                      </div>
+                  </div>
+                  <div className="item">
+                    <div id='boat-img' style={tripImage}></div>
+                      <div className="content">
+                          <div className="author">LUNDEV</div>
+                          <div className="title">DESIGN SLIDER</div>
+                          <div className="topic">ANIMAL</div>
+                          <div className="des">
+                              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut sequi, rem magnam nesciunt minima placeat, itaque eum neque officiis unde, eaque optio ratione aliquid assumenda facere ab et quasi ducimus aut doloribus non numquam. Explicabo, laboriosam nisi reprehenderit tempora at laborum natus unde. Ut, exercitationem eum aperiam illo illum laudantium?
+                          </div>
+                          <div className="buttons">
+                              <button>SEE MORE</button>
+                              <button>SUBSCRIBE</button>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              <div className="thumbnail">
+                  <div className="item">
+                      <div id='boat-img' style={tripImage}></div>
+                      <div className="content">
+                          <div className="title">
+                              Name Slider
+                          </div>
+                          <div className="description">
+                              Description
+                          </div>
+                      </div>
+                  </div>
+                  <div className="item">
+                      <div id='boat-img' style={tripImage}></div>
+                      <div className="content">
+                          <div className="title">
+                              Name Slider
+                          </div>
+                          <div className="description">
+                              Description
+                          </div>
+                      </div>
+                  </div>
+                  <div className="item">
+                    <div id='boat-img' style={tripImage}></div>
+                      <div className="content">
+                          <div className="title">
+                              Name Slider
+                          </div>
+                          <div className="description">
+                              Description
+                          </div>
+                      </div>
+                  </div>
+                  <div className="item">
+                    <div id='boat-img' style={tripImage}></div>
+                      <div className="content">
+                          <div className="title">
+                              Name Slider
+                          </div>
+                          <div className="description">
+                              Description
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              <div className="arrows">
+                  <button id="prev"> prev </button>
+                  <button id="next"> next </button>
+              </div>
+              <div className="time"></div>
+                </div>
+
+            {/* <div className="col-6" style={rectangleStyle}></div>
             <div className='col-6'>
               <div className='row-1' style={mostVisited}>
                 <p> MOST VISITED </p>
@@ -179,7 +295,7 @@ function Place() {
                 <p> Explore rugged hills, clear waters, and stunning views in Indonesia's Komodo National Park.
                   A hidden gem for adventurers and nature lovers.</p>
               </div>
-            </div>
+            </div> */}
           </div>
         </div>
       </section>
